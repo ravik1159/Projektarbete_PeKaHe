4b7a3fa1bcf18cf09235df64e597f4f873668eb6
Helena <helenalundstrom@hotmail.se>
2020-11-25 14:13:38 +0100

    created Arraylist ourContactBook andaAdded constructor in class contactbook

--- src/main/java/se/iths/contactdomain/ContactBook.java
+++ src/main/java/se/iths/contactdomain/ContactBook.java
@@ -1,4 +1,13 @@
 package se.iths.contactdomain;
 
+import java.util.ArrayList;
+
 public class ContactBook {
+
+    private ArrayList<Contact> ourContactBook;
+
+    public ContactBook() {
+        this.ourContactBook = new ArrayList<>();
+    }
+
 }
--- src/main/java/se/iths/storage/Storage.java
+++ src/main/java/se/iths/storage/Storage.java
@@ -1,4 +1,5 @@
 package se.iths.storage;
 
 public class Storage {
+
 }
