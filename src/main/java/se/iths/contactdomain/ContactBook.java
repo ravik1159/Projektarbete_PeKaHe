cd6c0e34a261692f27ed2ce3931922ba6978848d
Helena <helenalundstrom@hotmail.se>
2020-11-26 15:52:05 +0100

    Added method findContact in ContactBook

--- src/main/java/se/iths/contactdomain/ContactBook.java
+++ src/main/java/se/iths/contactdomain/ContactBook.java
@@ -12,4 +12,27 @@ public class ContactBook {
 
     public void addContact(Contact newContact) {
     }
+
+    private int findContact(String contactFirstName) {
+
+        for (int i = 0; i < ourContactBook.size(); i++) {
+            Contact contact = this.ourContactBook.get(i);
+            if (contact.getFirstName().equals(contactFirstName)) {
+                return i;
+            }
+        }
+        return -1;
+         /* Enhanced for-loop som jag vill testa sen när det finns sakern man kan testa med ;)
+
+        for(Contact index: ourContactBook){
+            Contact contact = index;
+            if(contact.getFirstName().equals(contactFirstName)){
+                return ourContactBook.indexOf(contact);
+            }
+        }
+        return -1;
+
+         */
+    }
+
 }
