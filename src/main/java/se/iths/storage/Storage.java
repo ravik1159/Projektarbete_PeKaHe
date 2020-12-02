4ae7edd57c3700f4f60875c218ae1f2936bda5b3
Petra Andreasson <petra.andreasson@iths.se>
2020-12-02 20:35:35 +0100

    Removed throws from writeToFile. Moved code from fileContainsContacts to loadFromFile to simplify. Removed throws and added ClassNotFoundException in catch clause in loadFromFile

--- src/main/java/se/iths/storage/Storage.java
+++ src/main/java/se/iths/storage/Storage.java
@@ -6,12 +6,10 @@ import java.util.ArrayList;
 
 public class Storage {
 
-    public void writeToFile(ArrayList<Contact> contactArrayList) throws Exception {
-
+    public void writeToFile(ArrayList<Contact> contactArrayList)  {
         try{
             FileOutputStream fileOutputStream = new FileOutputStream("contactsaves.txt");
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
-
             objectOutputStream.writeObject(contactArrayList);
             objectOutputStream.flush();
             objectOutputStream.close();
@@ -21,38 +19,39 @@ public class Storage {
             ioe.printStackTrace();
         }
     }
+//Petra: moved this method to loadFromFile. Delete the block if OK or uncomment and restore code as it was
+//    public boolean fileContainsContacts() throws Exception {
+//        File yourFile = new File("contactsaves.txt");
+//        yourFile.createNewFile(); // if file already exists, will do nothing
+//        if (yourFile.length() == 0) {
+//            // file does not contains contacts
+//            return false;
+//        } else {
+//            // if txt file is not empty, it will load the txt file
+//            return true;
+//        }
+//    }
 
-    public boolean fileContainsContacts() throws Exception {
-        File yourFile = new File("contactsaves.txt");
-        yourFile.createNewFile(); // if file already exists, will do nothing
-        if (yourFile.length() == 0) {
-            // file does not contains contacts
-            return false;
-        } else {
-            // if txt file is not empty, it will load the txt file
-            return true;
-        }
-    }
-
-    public ArrayList<Contact> loadFromFile() throws Exception{
-
+    public ArrayList<Contact> loadFromFile() {
         ArrayList<Contact> loadOurContactBook;
         try{
-            FileInputStream fileInputStream = new FileInputStream("contactsaves.txt");
-            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-
-            loadOurContactBook = (ArrayList<Contact>) objectInputStream.readObject();
-            objectInputStream.close();
-            fileInputStream.close();
+            File yourFile = new File("contactsaves.txt");
+            yourFile.createNewFile(); // if file already exists, will do nothing
+            if (yourFile.length() == 0) {
+                // file does not contains contacts
+                loadOurContactBook = new ArrayList<>();
+            } else {
+                FileInputStream fileInputStream = new FileInputStream(yourFile);
+                ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+                loadOurContactBook = (ArrayList<Contact>) objectInputStream.readObject();
+                objectInputStream.close();
+                fileInputStream.close();
+            }
             return loadOurContactBook;
 
-        } catch (IOException ioe){
+        } catch (IOException | ClassNotFoundException ioe){
             ioe.printStackTrace();
             return null;
-            //Lägga till Classnotfoundexception också?? Kanske file not found exception?
-            // //Se exempel i boken från Karen och videon av Pontus
         }
-
     }
-
 }
