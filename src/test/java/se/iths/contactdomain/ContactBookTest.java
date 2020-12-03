790ddc9d3c340bad81426e2aa3f72eb81ac19703
Petra Andreasson <petra.andreasson@iths.se>
2020-12-03 10:14:57 +0100

    Added asserts to removeContact test

--- src/test/java/se/iths/contactdomain/ContactBookTest.java
+++ src/test/java/se/iths/contactdomain/ContactBookTest.java
@@ -1,11 +1,32 @@
 package se.iths.contactdomain;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ContactBookTest {
+
+    private static ContactBook addedContacts;
+//    private static Contact cont1;
+//    private static Contact cont2;
+//    private static Contact cont3;
+
+    @BeforeAll
+    static void setup() {
+        List<Contact> testContacts = new ArrayList<>(); //Needed to be able to add Contacts to arraylist using the standard add method instead of our own addContact method
+        Contact cont1 = new Contact("Petra", "Andreasson", "077436436");
+        Contact cont2 = new Contact("Helena", "Lundström", "943743587");
+        Contact cont3 = new Contact("Karen", "Batjes", "73428465");
+        testContacts.add(cont1);
+        testContacts.add(cont2);
+        testContacts.add(cont3);
+        addedContacts = (ContactBook) testContacts;
+    }
 
     @BeforeEach
     void setUp() {
@@ -33,5 +54,8 @@ class ContactBookTest {
 
     @Test
     void removeContact() {
+        assertTrue(addedContacts.removeContact("Petra", "Andreasson"));
+        assertFalse(addedContacts.removeContact("Kurt","j342k!#"));
+        //assertEquals(2, );//expect, actual Vill testa arraylistans längd
     }
 }
